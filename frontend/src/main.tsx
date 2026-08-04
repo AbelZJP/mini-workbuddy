b5a245858f395a4b1dae2b877ff6ffb37e00c5e5
ouyangy007 <ouyangy007@gmail.com>
2026-08-04 18:35:49 +0800

    feat: 样式文件拆分

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
-import "./styles.css";
-import "./markdown.css";
+import "./styles.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
